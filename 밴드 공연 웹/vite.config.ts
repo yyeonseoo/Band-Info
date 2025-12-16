050ee15eacd8a9c830ca04c82966fd2d809ac6f4
LEE JI WOO <LEEJIWOO@MacBook-Air.local>
2025-12-17 00:21:23 +0900

    fix: update vite config base and fix build error

--- 밴드 공연 웹/vite.config.ts
+++ 밴드 공연 웹/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/band-info/',   // ⭐ 중요
   plugins: [react()],
   optimizeDeps: {
     include: ['xlsx'],
